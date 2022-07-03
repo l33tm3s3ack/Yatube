b3aab5508a9b052cb990ef25d5009405afac35e2
Андрей Каряев <andrei.karyaev@gmail.com>
2022-07-03 18:18:52 +0300

    PEP8

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -37,12 +37,12 @@ class PostFormTests (TestCase):
         cls.link_edit_redirect = reverse('posts:post_edit',
                                          kwargs={'post_id': cls.post.id})
         cls.simple_image = (
-             b'\x47\x49\x46\x38\x39\x61\x02\x00'
-             b'\x01\x00\x80\x00\x00\x00\x00\x00'
-             b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
-             b'\x00\x00\x00\x2C\x00\x00\x00\x00'
-             b'\x02\x00\x01\x00\x00\x02\x02\x0C'
-             b'\x0A\x00\x3B'
+            b'\x47\x49\x46\x38\x39\x61\x02\x00'
+            b'\x01\x00\x80\x00\x00\x00\x00\x00'
+            b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
+            b'\x00\x00\x00\x2C\x00\x00\x00\x00'
+            b'\x02\x00\x01\x00\x00\x02\x02\x0C'
+            b'\x0A\x00\x3B'
         )
         cls.image = SimpleUploadedFile(name='simple_image.jpg',
                                        content=cls.simple_image,
